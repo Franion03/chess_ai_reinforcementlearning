bfc61c52c4f7f891a023866aed3bcd36ca4b02ee
Fran Quiles <fran.quiles00@gmail.com>
2024-04-28 05:55:49 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FranReinforcement',
+    version='0.3',
+    packages=[ 'move_chess', 'capture_chess', 'real_chess'],
+    url='https://github.com/Franion03/chess_ai_reinforcementlearning',
+    license='MIT',
+    author='Fran Quiles',
+    author_email='fran.quiles00@gmail.com',
+    description='Collection of reinforcement learning algorithms, all applied to chess or chess related problems with tensorflow'
+)
